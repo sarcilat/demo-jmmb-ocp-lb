b8096f00f609cfa820403a684da982e8a71841f0
Sebastian Arcila Torres <arcilasebas@gmail.com>
2025-03-10 20:34:11 -0500

    hostname presentation adjustment

--- hostname.php
+++ hostname.php
@@ -1,3 +1,3 @@
 <?php
-  print "This instance runs on the POD with IP -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
+  print "This POD runs on the hostname -> " . gethostname() .", with this IP: " . $_SERVER['SERVER_ADDR'] . "\n";
 ?>  
